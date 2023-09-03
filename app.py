aa8422d98e0a08f0dcfb6c07d0df5146e5bb78d7
Firas.Al <96574769+F2691981@users.noreply.github.com>
2023-09-03 17:24:39 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,39 +1,32 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template,jsonify
+from sqlalchemy import text
+from database import engine
 
-app = Flask(__name__)  # how a script is invoked
+app = Flask(__name__)  # how a script is invok
 
-JOBS = [{
-  "id": 1,
-  "title": "Data Analyst",
-  "Location": "Beirut,Lebanon",
-  "Salary": "$ 1000.00"
-}, {
-  "id": 2,
-  "Location": "Remote",
-  "title": "Data Scientist",
-  "Salary": "$ 1500.00"
-}, {
-  "id": 3,
-  "Location": "Onsite",
-  "title": "Back-end Engineer",
-  "Salary": "$ 1300.00"
-}, {
-  "id": 4,
-  "Location": "Hybrid",
-  "title": "Front-end Engineer",
-  "Salary": "$ 1300.00"
-}]
+JOBS = [{"jobs"},{"jobs"}]
+jobs = JOBS
 
+def load_jobs_from_db():
+  with engine.connect() as conn:
+    result = conn.execute(text("select * from jobs"))
+    jobs = result.mappings().all()
+    print(jobs)
+    
 
-@app.route("/jobs")
+@app.route("/")
+def hello_world():
+  jobs = load_jobs_from_db()
+  return render_template("Home.html",
+                         jobs = JOBS,
+                         Company_Name="Careers4-all")
+  
+
+@app.route("/api/jobs")
 def list_jobs():
   return jsonify(JOBS)
 
 
-@app.route("/")
-def hello_world():
-  return render_template("Home.html", jobs=JOBS, Company_Name="Careers4-all")
-
-
 if __name__ == "__main__":
-  app.run(host='0.0.0.0', debug=True)
+  app.run(host='0.0.0.0', debug=True)  
+
