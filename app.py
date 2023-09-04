6c9023e36a2b8d0fb97282c06ffff9b4af1a6044
Firas.Al <96574769+F2691981@users.noreply.github.com>
2023-09-04 17:29:14 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,30 +1,24 @@
 from flask import Flask, render_template,jsonify
-from sqlalchemy import text
-from database import create_engine,engine
+from database import load_jobs_from_db
 
 app = Flask(__name__)  # how a script is invok
-def load_jobs_from_db():
-  with engine.connect() as conn:
-    result = conn.execute(text("select * from jobs"))
-    jobs=[]
-    for rows in result.all():
-      jobs.append(rows)
-    return jobs
 
-JOBS = load_jobs_from_db()
+
+JOBS = load_jobs_from_db(id)
 jobs = JOBS
 
 
 @app.route("/")
 def hello_world():
-  jobs = load_jobs_from_db()
+  jobs = load_jobs_from_db(id)
   return render_template("Home.html",
-                         jobs = JOBS,
+                         jobs = jobs,
                          Company_Name="Careers4-all.org")
   
 @app.route("/api/jobs")
 def list_jobs():
-  return jsonify(JOBS)
+  jobs = load_jobs_from_db()
+  return jsonify(jobs)
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)  
