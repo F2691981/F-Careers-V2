a43bba69d83d6af4531a3b3a426cb0e47701392d
Firas.Al <96574769+F2691981@users.noreply.github.com>
2023-09-04 22:57:25 +0300

    Update database.py

--- database.py
+++ database.py
@@ -1,8 +1,1 @@
-from sqlalchemy import create_engine
 
-db_connection_string = "mysql+pymysql://l6tbwccknb1jb6knd5wn:pscale_pw_Magv1J5sLaICLmVGL4cVWbvV2jcuPMnTSanKlozquVV@aws.connect.psdb.cloud/fcareers?charset=utf8mb4"
-
-engine = create_engine(db_connection_string,
-                       connect_args={"ssl": {
-                         "ssl_ca": "/etc/ssl/cert.pem"
-                       }})
