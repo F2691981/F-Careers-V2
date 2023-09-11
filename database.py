4159d7e557600f7af05872e4c4eaf786b25ca322
Firas.Al <96574769+F2691981@users.noreply.github.com>
2023-09-11 09:32:49 +0300

    Create database.py

---
+++ database.py
@@ -0,0 +1,12 @@
+import os
+from sqlalchemy import create_engine,text
+
+db_connection_string = os.environ['DB_CONNECTION_STRING']
+
+engine = create_engine(
+  db_connection_string,
+  connect_args={
+    "ssl": {
+      "ssl_ca": "/etc/ssl/cert.pem"
+    }
+  })
