da7e7b9a1c39f4e7a95ba95d99bd0b656e969b5a
Firas.Al <96574769+F2691981@users.noreply.github.com>
2023-09-11 10:35:12 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,19 @@
 from flask import Flask, render_template,jsonify
-from database import load_jobs_from_db
+from database import engine
+from sqlalchemy import text
 
 app = Flask(__name__)  # how a script is invok
 
+
+def load_jobs_from_db():
+  with engine.connect() as conn:
+    result = conn.execute(
+      text("select title, location, salary,currency from jobs"))
+    jobs = []
+    for row in result.all():
+      jobs.append(row._mapping)
+    return jobs
+    
 JOBS = load_jobs_from_db()
 jobs = JOBS
 
