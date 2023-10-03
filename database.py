42d9a1849d7b545dab0e010eca69ed649e35168b
Firas.Al <96574769+F2691981@users.noreply.github.com>
2023-10-03 13:01:34 +0300

    Update database.py

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine,text
 
 
 
-db_connection_string = "mysql+pymysql://l6tbwccknb1jb6knd5wn:pscale_pw_Magv1J5sLaICLmVGL4cVWbvV2jcuPMnTSanKlozquVV@aws.connect.psdb.cloud/fcareers?charset=utf8mb4"
+db_connection_string = ""
 
 
 engine = create_engine(
